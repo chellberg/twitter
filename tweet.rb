3361c0136089e767f7ae1fbf38cd3df4652cfcf9
Chellberg <corinchellberg@gmail.com>
2013-07-07 14:33:31 -0400

    Remove API Keys

--- tweet.rb
+++ tweet.rb
@@ -2,15 +2,13 @@ require 'rubygems'
 require 'oauth'
 require 'json'
 
-# You will need to set your application type to
-# read/write on dev.twitter.com and regenerate your access
-# token.  Enter the new values here:
+# keys removed after a friend helpfully pointed out the obvious security risks
 consumer_key = OAuth::Consumer.new(
-  "k7cxoVqy2GwJGQYG7IMxgg",
-  "hR5X6NFpyovbq22Yr5yqWCftGupmpHDcLnd8QoWcjg")
+  "",
+  "")
 access_token = OAuth::Token.new(
-  "7898992-ly9zLY1jB3xe0E2QkQs0MphB231rUiJXUjk9RA8pMc",
-  "1oWIlNCYjOFDjX7OW9qeOeANh4rqy72TqWPAgznE8")
+  "",
+  "")
 
 # Note that the type of request has changed to POST.
 # The request parameters have also moved to the body
